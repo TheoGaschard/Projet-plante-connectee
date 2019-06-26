8c782677db1c1a2bd44a36a716e0b80644a53ca7
Gaschard <46054102+TheoGaschardYnov@users.noreply.github.com>
2019-06-27 00:20:44 +0200

    Add files via upload

---
+++ Code Arduino/Projet.ino
@@ -0,0 +1,95 @@
+/*
+ Programme pour le projet plante conectée.
+
+
+
+
+
+ */
+
+#include <DHT.h> // Add of the DHT library.
+#include <DHT_U.h> // Add of the DHT_U library.
+#include <WiFi.h>
+#include <ThingerWifi.h> // Adding the wi-fi library.
+#include "Wire.h"
+#include <Sparkfun_APDS9301_Library.h> // Adding the light sensor library.
+
+#define _DEBUG_
+#define _DISABLE_TLS_
+#define THINGER_USE_STATIC_MEMORY
+#define THINGER_STATIC_MEMORY_SIZE 512
+
+#define MOISTURE_PIN A4 //PIN connected to the SEN0193 sensor.
+#define DHTPIN A2 // PIN connected to the DHT11 sensor.
+#define LIGHT_PIN 2 // Pin linked to the APDS9301 light sensor.
+
+// We define the connection options.
+#define USERNAME "GroupeGTYnov"
+#define DEVICE_ID "ARDUIN"
+#define DEVICE_CREDENTIAL "r5Boe1Ige&#j"
+#define SSID "XperiaXA2" // Name of the wi-fi network.
+#define SSID_PASSWORD "23456789876" // Password of the wi-fi network.
+
+ThingerWifi thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL); // Establishing connection with the wi-fi network.
+
+APDS9301 apds;
+
+
+int moistureValue = 0;
+int humidityValue = 0;  
+int temperatureValue = 0;
+int luminosityValue = 0;
+
+DHT dht(DHTPIN, DHT11);
+
+
+void setup(){
+  
+  pinMode(MOISTURE_PIN, INPUT);
+  
+  dht.begin();
+  
+  thing.add_wifi(SSID, SSID_PASSWORD); // Configure the wi-fi network.
+
+  delay(50); // The CCS811 wants a brief delay after startup.
+  Serial.begin(115200); // Open the serial monitor.
+  Wire.begin();
+
+  // APDS9301 sensor setup.
+  apds.begin(0x39); 
+  apds.setLowThreshold(0); // Sets the low threshold to 0, effectively
+  //  disabling the low side interrupt.
+  apds.setHighThreshold(50); // Sets the high threshold to 500. This
+  //  is an arbitrary number I pulled out of thin
+  //  air for purposes of the example. When the CH0
+  //  reading exceeds this level, an interrupt will
+  //  be issued on the INT pin.
+  apds.enableInterrupt(APDS9301::INT_ON); // Enable the interrupt.
+  apds.clearIntFlag();
+
+  // Interrupt setup.
+  pinMode(LIGHT_PIN, INPUT_PULLUP);
+  Serial.println(apds.getLowThreshold());
+  Serial.println(apds.getHighThreshold());
+
+  thing["humidity"] >> humidityValue;
+  thing["moisture"] >> moistureValue;
+  thing["temperature"] >> temperatureValue;
+  thing["luminosity"] >> luminosityValue;
+
+}
+
+
+void loop()
+{
+  moistureValue = analogRead(MOISTURE_PIN); // We collect the moisture of the dirt.
+  humidityValue = dht.readHumidity(); // We collect the humidity of the air.
+  temperatureValue = dht.readTemperature(); // We collect the temperature of the air.
+  luminosityValue = apds.readCH0Level(); // We collect the luminosity.
+  
+  thing.handle();
+  apds.clearIntFlag();
+  delay(1000);
+
+
+}
---
+++ Code Python/progprojet.py
@@ -0,0 +1,172 @@
+## Programme python du projet plante connectée
+
+
+import requests
+import json
+
+
+            ## Connexion à l'API :
+            
+print("\nApplication plante connectée\n")
+
+url_api = "https://api.thinger.io/"
+parametre = "oauth/token"
+
+main_api = url_api+parametre
+
+
+username = input("Nom d'utilisateur : ")
+motdepasse = input("Mot de passe : ")
+
+mydata = {
+    "Content-Type":"application/x-www-form-urlencoded",
+    "grant_type":"password",
+    "username": username,
+    "password": motdepasse
+    }
+
+
+
+        ## interogation de l'api
+        
+objet_api = requests.post(main_api, data=mydata)
+jsondata = objet_api.json()
+errorcode = objet_api.status_code
+
+
+while (errorcode != 200):
+    print("erreur ", errorcode)
+    print("Une erreur s'est produite, identifiant ou mot de passe incorrect.")
+    username = input("Nom d'utilisateur : ")
+    motdepasse = input("Mot de passe : ")
+    
+    mydata["username"] = username
+    mydata["password"] = motdepasse
+    
+    
+    objet_api = requests.post(main_api, data=mydata)
+    jsondata = objet_api.json()
+    errorcode = objet_api.status_code
+
+
+
+## print(jsondata)
+## print("")
+
+token = jsondata["access_token"]
+
+
+parametre = "v1/users/" + username + "/devices?authorization=" + token
+main_api = url_api+parametre
+
+
+devices = requests.get(main_api).json()
+print("Liste des devices :")
+for device in devices:
+    print(device["device"])
+
+choixdevice = input("Devices disponibles : \n")
+print("")
+
+## print(devices)
+## print("")
+
+onpeutcontinuer = 0 
+for device in devices:
+    if(choixdevice == device["device"]):
+        print(device["device"]," :", end ='' )
+        connectionstatus = device["connection"]
+        if (connectionstatus["active"] == False):
+            print("non connecté")
+        else:
+            print("connecté")
+            onpeutcontinuer = 1
+      
+           
+if (onpeutcontinuer == 1):
+    
+            ## Récupération depuis Thinger.io
+    
+    ressource_map = ["luminosity","temperature","humidity","moisture",] 
+    
+    
+    valeurs = []
+    for ressource in ressource_map:
+        
+        parametre = "v2/users/" + username + "/devices/" + choixdevice + "/" + ressource + "?authorization="+ token
+        main_api = url_api+parametre
+        stats = requests.get(main_api).json()
+        ## print(stats)
+        if "out" in stats:
+            valeurs.append(stats["out"])
+        
+        ##stats correspond à :  {'out': 24}
+   
+    ## print(valeurs)
+    
+    
+            ## Récupération depuis la base de données :
+
+    plante = "a"
+    base = [[0,0],[0,0]]
+    
+    separateur = ";"
+    
+    def readfilesep(nom, separateur):
+        base = []
+        fichier = open(nom, 'r')
+        for line in fichier: 
+            petit_tab = []
+            petit_tab = line.replace('\n','').split(separateur)
+            base.append(petit_tab)
+            
+        fichier.close()
+        return base
+    
+    Donnees = readfilesep("BDD.txt", separateur)
+    ## print(Donnees)
+    
+    
+            ## Traitement des données :
+    
+    plante = input("Quelle est votre plante ? \n")
+    
+    for entree in Donnees:
+        if (plante == entree[0]):
+            print ("\nInformations sur : ", entree[0]) 
+            print ("L'humidité de l'air idéale est ", entree[3])
+            print ("L'humidité du sol idéale est ", entree[4])
+            print ("La température idéale est ", entree[2])
+            print ("La luminosité idéale est ", entree[1])
+            print ("\n")
+            print ("Données actuelles de : ", plante) 
+            print ("L'humidité de l'air actuelle est ", valeurs[0])
+            print ("L'humidité du sol actuelle est ", valeurs[1])
+            print ("La température actuelle est ", valeurs[2])
+            print ("La luminosité actuelle est ", valeurs[3])
+            
+            
+            ## Informations suppémentaires:
+    
+    veplus = input("Voulez vous des informations complémentaires ? (oui ou non) \n")
+    
+    if (veplus == "oui"):
+        
+        infossup = readfilesep("InfosPlantes.txt", separateur)
+        
+        for nom, categorie, info, date in infossup:
+            if (plante == nom):
+                print ("Informations supplémentaires sur le ", nom) 
+                print ("Elle fait partie du groupe des ", categorie)
+                print (info, ".") 
+                print ("Elle peut être plantée ", date) 
+        
+        
+        
+print ("\n  Merci d'avoir utiliser notre application ")        
+        
+        
+        
+        
+        
+    
